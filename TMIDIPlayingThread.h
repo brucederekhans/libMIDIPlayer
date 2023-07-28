8f80441c5d1c73c5c0aea8b736b00f4ee9ce1d6e
brucederekhans <brucederekhans@gmail.com>
2023-07-28 09:46:55 +0800

    define volumePercentage as member of TMIDIPlayingThread

--- TMIDIPlayingThread.h
+++ TMIDIPlayingThread.h
@@ -16,6 +16,7 @@ protected:
 	void __fastcall Execute();
 private:
 	char filename[MAX_PATH];
+	unsigned char volumePercentage;
 };
 //---------------------------------------------------------------------------
 #endif
