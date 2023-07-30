49752bc69ef396dcab61ebceff2dddb3c5b9f73d
brucederekhans <brucederekhans@gmail.com>
2023-07-30 22:49:32 +0800

    define midiTrackHeader as member of TMIDIPlayingThread

--- TMIDIPlayingThread.h
+++ TMIDIPlayingThread.h
@@ -22,6 +22,7 @@ private:
 	bool isTrackHeadersValid;
 	HMIDIOUT hMIDIOut;
 	TMIDI midi;
+	TMIDITrackHeader midiTrackHeader;
 };
 //---------------------------------------------------------------------------
 #endif
