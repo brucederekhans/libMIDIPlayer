0ac8390692e89a1d8d1b18db3cc20353fd129d26
brucederekhans <brucederekhans@gmail.com>
2023-08-06 15:03:34 +0800

    implement readUShortFromMIDIFile()

--- TMIDI.cpp
+++ TMIDI.cpp
@@ -8,3 +8,10 @@
 //---------------------------------------------------------------------------
 const char MThd[5] = "MThd";
 //---------------------------------------------------------------------------
+unsigned short readUShortFromMIDIFile(FILE * pMIDIFile)
+{
+	unsigned char hiByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+	unsigned char loByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+	return static_cast<unsigned short>(MAKEWORD(loByte, hiByte));
+}
+//---------------------------------------------------------------------------
