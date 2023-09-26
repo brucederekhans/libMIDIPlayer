5ec4f962ce2dcaf3007d39e8568db876b8c38c26
brucederekhans <brucederekhans@gmail.com>
2023-09-27 04:25:34 +0800

    reset pData the member of all tracks

--- TMIDIPlayingThread.cpp
+++ TMIDIPlayingThread.cpp
@@ -389,6 +389,15 @@ void __fastcall TMIDIPlayingThread::Execute()
 
 					midi.isPlaying = 0;
 				}
+
+				if(this->isTrackHeadersValid)
+				{
+					unsigned short iTrack;
+					for(iTrack = 0; iTrack < midi.countTracks; iTrack++)
+					{
+						midiTrackHeaders[iTrack].pData = nullptr;
+					}
+				}
 			}
 			catch(int errCode)
 			{
